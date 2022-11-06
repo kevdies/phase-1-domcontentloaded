2583eac6cde2690f6cad8885511be141e6d6cf5d
kevin diesenberg <kdiesenb@gmail.com>
2022-11-06 10:56:35 -0500

    commit tree a8bad30e77925e5263b5dd70ac34dd51f27fc127

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    changeText();
+});
+
+function changeText() {
+    let p = document.getElementById("text")
+
+        p.textContent = "This is really cool!";
+}
